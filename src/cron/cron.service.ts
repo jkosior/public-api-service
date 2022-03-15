8b49da7a9a7afddd13a72f2d7f3e456c411dc0ae
Jakub <jakubjkosior@gmail.com>
2022-03-15 12:40:06 +0100

    add cron service

--- src/cron/cron.service.ts
+++ src/cron/cron.service.ts
@@ -1,4 +1,21 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PublicApiConnectorService } from '../public-api-connector/public-api-connector.service';
+import { Cron, CronExpression, Timeout } from '@nestjs/schedule';
 
 @Injectable()
-export class CronService {}
+export class CronService {
+  constructor(
+    @Inject(PublicApiConnectorService)
+    private readonly apiConnectorService: PublicApiConnectorService,
+  ) {}
+
+  @Timeout(1000)
+  async handleStartupUpdate() {
+    await this.apiConnectorService.updateApis();
+  }
+
+  @Cron(CronExpression.EVERY_HOUR)
+  async handleHourlyUpdate() {
+    await this.apiConnectorService.updateApis();
+  }
+}
