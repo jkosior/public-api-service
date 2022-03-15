d8a92290ae8f90ae6783bdd46aea017a4f4897a5
Jakub <jakubjkosior@gmail.com>
2022-03-15 09:52:29 +0100

    format

--- src/config/public-api.config.ts
+++ src/config/public-api.config.ts
@@ -1,4 +1,4 @@
 export default {
   baseURL: 'https://api.publicapis.org/',
   timeout: process.env.PUBLIC_API_TIMEOUT || 5000,
-}
+};
--- src/public-api-connector/public-api-connector.service.ts
+++ src/public-api-connector/public-api-connector.service.ts
@@ -1,9 +1,4 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class PublicApiConnectorService {
-
-
-
-
-}
+export class PublicApiConnectorService {}
